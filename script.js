ec5a2174e618432b9ca85a7e11388631c6966d6b
BC Ubuntu <balthazar.coquard@gmail.com>
2024-07-07 13:00:50 +0200

    Hide secret number

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 let highscore = 0;
 
 const displayMessage = function (message) {
@@ -43,6 +43,7 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     // document.querySelector('.message').textContent = '🎉 Correct Number!';
     displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
